fa28b9daf1262f2155bd33456649a6db3d9b940f
Gustavo bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2022-12-09 14:09:51 -0300

    Aula livro inicial

---
+++ Aula/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+//Variáveis 
+        int idade = 10;
+        double pi = 3.14;
+        boolean verdade = true;
+        boolean menorIdade = idade < 18;
+// char só guarda uma letra
+        char letra;
+        letra = 'a';
+        System.out.println(letra);
+    }
+}

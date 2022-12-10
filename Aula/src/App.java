99ad29bf8031791ebe553a516f61fa05efa9bcc7
Gustavo bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2022-12-10 16:46:33 -0300

    Exercicio1

--- Aula/src/App.java
+++ Aula/src/App.java
@@ -14,5 +14,23 @@ public class App {
         System.out.print(pi);
         System.out.println(verdade);
         System.out.println("Teste de imprimir ");
+//Arredondando número quebrado para inteiro
+        double d2 = 3.14;
+        int d2inteiro = (int) d2;
+        System.out.println(d2inteiro);
+//Variável para números longos
+        long enorme = 123456789;
+        System.out.println(enorme);
+//While
+        while (enorme > 123456780) {
+            System.out.println(enorme);
+            enorme = enorme - 1;
+        }     
+//For
+        System.out.println("Contando de 0 até 10");
+        for (int i = 0; i < 11; i = i + 1) {
+            System.out.print("número ");
+            System.out.println(i);
+        }
     }
 }

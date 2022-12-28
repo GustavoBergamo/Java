e9684e706a69e07165d78c342e23ab661aa4fb87
Gustavo bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2022-12-28 12:07:17 -0300

    aula23

---
+++ TiagoAguiar/Aula1/SetterGetter/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        User userA = new User();
+        userA.setFirstName("Abraão");
+        System.out.println(userA.getFirstName());
+
+    }
+}
---
+++ TiagoAguiar/Aula1/SetterGetter/src/User.java
@@ -0,0 +1,17 @@
+//Não é interessante deixar variáveis públicas,
+//Então cria-se comportamentos seting/geting
+public class User {
+    private String firstName;
+    private String lastName;
+
+    //Funções para mudar os parâmetros
+    //Abaixo um SETTER
+    public void setFirstName(String firstName){
+        //acessa o firstName dessa classe para armazenar o que veio lá do main () e armazena tudo maiúsculo
+        this.firstName = firstName.toUpperCase();
+    }
+    //Abaixo um GETTER
+    public String getFirstName(){
+        return this.firstName;//vai retornar o que armazenou no firstName (agora tudo em maiúsculo)
+    }
+}

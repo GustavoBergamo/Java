8dc36a81b5e9ce27ceee54313d038568fbb9ba53
Gustavo bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2023-01-12 12:46:31 -0300

    Lotof att
    att

--- Lotofacil/src/Lotofacil.java
+++ Lotofacil/src/Lotofacil.java
@@ -16,29 +16,17 @@ public class Lotofacil {
         Random generateD = new Random();
 
         int[] a = new int[25];
-    //    int[] b = new int[25];
-    //    int[] c = new int[25];
-    //    int[] d = new int[25];
         int[] aa = new int[26];
+        int[] b = new int[25];
         int i = 0;
         a[0] = 0;
-    //    b[0] = 0;
-    //    c[0] = 0;
-    //    d[0] = 0;
         int number;
 
 //while (true) resulta num looping infinito;
 //for(;;) -> também resulta num looping infinito
         List sorteio1 = new ArrayList();
-//        List sorteio2 = new ArrayList();
-//        List sorteio3 = new ArrayList();
-//        List sorteio4 = new ArrayList();
         List sorteio5 = new ArrayList();
-//        List sorteio6 = new ArrayList();
-//        List sorteio7 = new ArrayList();
-//        List sorteio8 = new ArrayList();
         List sorteio9 = new ArrayList();
-
 
         while (i < 15) {
             number = generate.nextInt(26);
@@ -50,47 +38,8 @@ public class Lotofacil {
             }
         }
 
-//        i = 0;
-
-//        while (i < 15) {
-//            number = generate.nextInt(26);
-//            if (number == b[0] || number == b[1] || number == b[2] || number == b[3] || number == b[4] || number == b[5] || number == b[6] || number == b[7] || number == b[8] || number == b[9] || number == b[10] || number == b[11] || number == b[12] || number == b[13] || number == b[14]) {
-//                number = generateB.nextInt(26);
-//            } else {
-//                b[i] = number;
-//                i++;
-//            }
-//        }
-
-//        i = 0;
-
-//        while (i < 15) {
-//            number = generate.nextInt(26);
-//            if (number == c[0] || number == c[1] || number == c[2] || number == c[3] || number == c[4] || number == c[5] || number == c[6] || number == c[7] || number == c[8] || number == c[9] || number == c[10] || number == c[11] || number == c[12] || number == c[13] || number == c[14]) {
-//                number = generateC.nextInt(26);
-//            } else {
-//                c[i] = number;
-//                i++;
-//            }
-//        }
-
-//        i = 0;
-
-//        while (i < 15) {
-//            number = generate.nextInt(26);
-//            if (number == d[0] || number == d[1] || number == d[2] || number == d[3] || number == d[4] || number == d[5] || number == d[6] || number == d[7] || number == d[8] || number == d[9] || number == d[10] || number == d[11] || number == d[12] || number == d[13] || number == d[14]) {
-//                number = generateD.nextInt(26);
-//            } else {
-//                d[i] = number;
-//                i++;
-//            }
-//        }
-
         //colocando números em ordem.
         Arrays.sort(a);
-//        Arrays.sort(b);
-//        Arrays.sort(c);
-//        Arrays.sort(d);
 
         //Colocando números em List's
         for (i = 1; i <= 24; ++i) {
@@ -99,49 +48,12 @@ public class Lotofacil {
             }
         }
 
-//        for (i = 1; i <= 24; ++i) {
-//            if (b[i] != 0) {
-//                sorteio2.add(b[i]);
-//            }
-//        }
-
-//        for (i = 1; i <= 24; ++i) {
-//            if (c[i] != 0) {
-//                sorteio3.add(c[i]);
-//            }
-//        }
-
-//        for (i = 1; i <= 24; ++i) {
-//            if (d[i] != 0) {
-//                sorteio4.add(d[i]);
-//            }
-//        }
-
-
         //criando espelhos
         for (i = 1; i <= 25; i++) {
             if (!sorteio1.contains(i)) {
                 sorteio5.add(i);
             }
         }
-
-//        for (i = 1; i <= 25; i++) {
-//            if (!sorteio2.contains(i)) {
-//                sorteio6.add(i);
-//            }
-//        }
-
-//        for (i = 1; i <= 25; i++) {
-//            if (!sorteio3.contains(i)) {
-//                sorteio7.add(i);
-//            }
-//        }
-
-//        for (i = 1; i <= 25; i++) {
-//            if (!sorteio4.contains(i)) {
-//                sorteio8.add(i);
-//            }
-//       }
 
         // Adicionando 5 números aleatórios que faltam em sorteio5,6,7 e 8
         i = 0;
@@ -168,7 +80,7 @@ public class Lotofacil {
         i = 1;
         while (i < 5) {
             number = generate.nextInt(26);
-            if (!sorteio9.contains(number)) {
+            if (!sorteio9.contains(number) && aa[0] == number || aa[1] == number || aa[2] == number || aa[3] == number) {
                 number = generate.nextInt(26);
             } else {
                 aa[i] = number;
@@ -176,41 +88,11 @@ public class Lotofacil {
             }
         }
 
+// removendo os 4 números sorteados acima do Jogo 3
 
-/*        i = 0;
-        while (i < 5) {
-            number = generate.nextInt(26);
-            while (sorteio6.contains(number) || number == 0) {
-                number = generateD.nextInt(26);
-            }
-            sorteio6.add(number);
-            i++;
-        }
 
-        i = 0;
-        while (i < 5) {
-            number = generate.nextInt(26);
-            while (sorteio7.contains(number) || number == 0) {
-                number = generateD.nextInt(26);
-            }
-            sorteio7.add(number);
-            i++;
-        }
-
-        i = 0;
-        while (i < 5) {
-            number = generate.nextInt(26);
-            while (sorteio8.contains(number) || number == 0) {
-                number = generateD.nextInt(26);
-            }
-            sorteio8.add(number);
-            i++;
-        }*/
 //Colocando sorteio5,6,7 e 8 em ordem
         Collections.sort(sorteio5);
-/*        Collections.sort(sorteio6);
-        Collections.sort(sorteio7);
-        Collections.sort(sorteio8);*/
         Collections.sort(sorteio9);
         //imprimindo sorteio e arrumados
 
@@ -227,39 +109,6 @@ public class Lotofacil {
         System.out.println();
         System.out.println();
 
-/*        i = 0;
-        while (i < 15) {
-            if ((int) sorteio2.get(i) < 10) {
-                System.out.print(sorteio2.get(i) + "   ");
-            } else {
-                System.out.print(sorteio2.get(i) + "  ");
-            }
-            i++;
-        }
-        System.out.println();
-
-        i = 0;
-        while (i < 15) {
-            if ((int) sorteio3.get(i) < 10) {
-                System.out.print(sorteio3.get(i) + "   ");
-            } else {
-                System.out.print(sorteio3.get(i) + "  ");
-            }
-            i++;
-        }
-        System.out.println();
-
-        i = 0;
-        while (i < 15) {
-            if ((int) sorteio4.get(i) < 10) {
-                System.out.print(sorteio4.get(i) + "   ");
-            } else {
-                System.out.print(sorteio4.get(i) + "  ");
-            }
-            i++;
-        }
-        System.out.println();*/
-
         i = 0;
         System.out.println("Jogo 2: ");
         while (i < 15) {
@@ -270,7 +119,6 @@ public class Lotofacil {
             }
             i++;
         }
-        System.out.println();
         System.out.println();
 
         i = 0;
@@ -285,162 +133,6 @@ public class Lotofacil {
         }
         System.out.println();
         System.out.println();
- /*
-        i = 0;
-        while (i < 15) {
-            if ((int) sorteio6.get(i) < 10) {
-                System.out.print(sorteio6.get(i) + "   ");
-            } else {
-                System.out.print(sorteio6.get(i) + "  ");
-            }
-            i++;
-        }
-        System.out.println();
-
-        i = 0;
-        while (i < 15) {
-            if ((int) sorteio7.get(i) < 10) {
-                System.out.print(sorteio7.get(i) + "   ");
-            } else {
-                System.out.print(sorteio7.get(i) + "  ");
-            }
-            i++;
-        }
-        System.out.println();
-
-        i = 0;
-        while (i < 15) {
-            if ((int) sorteio8.get(i) < 10) {
-                System.out.print(sorteio8.get(i) + "   ");
-            } else {
-                System.out.print(sorteio8.get(i) + "  ");
-            }
-            i++;
-        }
-        //Desenhando
-        System.out.println();
-        System.out.println("________________");
-        i = 1;
-        while (i < 6) {
-            if (sorteio1.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|0" + i);
-            }
-            i++;
-        }
-        System.out.println("|");
-
-        i = 6;
-        while (i < 10) {
-            if (sorteio1.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|0" + i);
-            }
-            i++;
-        }
-        if (sorteio1.contains(10)) {
-            System.out.println("|XX|");
-        } else {
-            System.out.println("|10|");
-        }
-        i = 11;
-        while (i < 16) {
-            if (sorteio1.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|" + i);
-            }
-            i++;
-        }
-        System.out.println("|");
-
-        i = 16;
-        while (i < 21) {
-            if (sorteio1.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|" + i);
-            }
-            i++;
-        }
-        System.out.println("|");
-
-        i = 21;
-        while (i < 26) {
-            if (sorteio1.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|" + i);
-            }
-            i++;
-        }
-        System.out.println("|");
-
-
-        System.out.println();
-        System.out.println("________________");
-        i = 1;
-        while (i < 6) {
-            if (sorteio2.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|0" + i);
-            }
-            i++;
-        }
-        System.out.println("|");
-
-        i = 6;
-        while (i < 10) {
-            if (sorteio2.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|0" + i);
-            }
-            i++;
-        }
-        if (sorteio2.contains(10)) {
-            System.out.println("|XX|");
-        } else {
-            System.out.println("|10|");
-        }
-        i = 11;
-        while (i < 16) {
-            if (sorteio2.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|" + i);
-            }
-            i++;
-        }
-        System.out.println("|");
-
-        i = 16;
-        while (i < 21) {
-            if (sorteio2.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|" + i);
-            }
-            i++;
-        }
-        System.out.println("|");
-
-        i = 21;
-        while (i < 26) {
-            if (sorteio2.contains(i)) {
-                System.out.print("|XX");
-            } else {
-                System.out.print("|" + i);
-            }
-            i++;
-        }
-        System.out.println("|");
-*/
-
-
     }
 }
 //while pode ser substituido por for

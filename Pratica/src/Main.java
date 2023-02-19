55a166d78478f88a0c25618c260cd657d6e97dbe
Gustavo Bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2023-02-18 22:45:02 -0300

    main

---
+++ Pratica/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int x = 0;
+        int y = 0;
+        System.out.println(x + "foi o x");
+        System.out.println(y + "foi o y");
+    }
+}

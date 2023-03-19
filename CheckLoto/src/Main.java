5e935f5a20877e607e97a40b755732a939c2b8e6
Gustavo Bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2023-03-19 16:53:02 -0300

    lotofacil arrumado

--- CheckLoto/src/Main.java
+++ CheckLoto/src/Main.java
@@ -1,14 +1,16 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Integer j1[] = {2, 3, 4, 5, 7, 9, 10, 11, 12, 13, 15, 16, 18, 21, 25};
         Integer j2[] = {1, 2, 6, 7, 8, 9, 11, 14, 17, 19, 20, 21, 22, 23, 24};
         Integer j3[] = {1, 4, 5, 8, 10, 12, 13, 14, 15, 16, 17, 19, 20, 23, 24};
-        Integer j4[] = {2, 3, 4, 5, 7, 9, 11, 12, 14, 17, 19, 21, 22, 23, 25};
+        Integer j4[] = {1, 3, 7, 8, 10, 13, 14, 17, 18, 19, 20, 21, 23, 24, 25};
 
         //Colocar aqui abaixo os numeros sorteados, devem ser 15 para nao gerar erro.
-        Integer sorteio[] = {1, 2, 3, 5, 9, 10, 12, 13, 15, 16, 17, 19, 23, 24, 25};
+        Integer sorteio[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
         List<Integer> l1 = Arrays.asList(j1);
         List<Integer> l2 = Arrays.asList(j2);
@@ -17,7 +19,12 @@ public class Main {
 
         int p1 = 0, p2 = 0, p3 = 0, p4 = 0;
 
+        System.out.println("Digite os números sorteados");
+        Scanner a = new Scanner(System.in);
+
         for (int i = 0; i < 15; i++){
+
+            sorteio[i] = a.nextInt();
             if (l1.contains(sorteio[i])){
                 p1++;
             }
@@ -30,11 +37,10 @@ public class Main {
             if (l4.contains(sorteio[i])){
                 p4++;
             }
+            System.out.println("jogo 1: " + p1 + " pontos");
+            System.out.println("jogo 2: " + p2 + " pontos");
+            System.out.println("jogo 3: " + p3 + " pontos");
+            System.out.println("jogo 4: " + p4 + " pontos");
         }
-        System.out.println("jogo 1: " + p1 + " pontos");
-        System.out.println("jogo 2: " + p2 + " pontos");
-        System.out.println("jogo 3: " + p3 + " pontos");
-        System.out.println("jogo 4: " + p4 + " pontos");
-
     }
 }

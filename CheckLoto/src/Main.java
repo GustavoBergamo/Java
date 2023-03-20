d7f2a5ffba1876b817ef1282047ddfb0c0fbda98
Gustavo Bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2023-03-19 21:27:57 -0300

    entrada surpresinha

--- CheckLoto/src/Main.java
+++ CheckLoto/src/Main.java
@@ -7,8 +7,14 @@ public class Main {
         Integer j1[] = {2, 3, 4, 5, 7, 9, 10, 11, 12, 13, 15, 16, 18, 21, 25};
         Integer j2[] = {1, 2, 6, 7, 8, 9, 11, 14, 17, 19, 20, 21, 22, 23, 24};
         Integer j3[] = {1, 4, 5, 8, 10, 12, 13, 14, 15, 16, 17, 19, 20, 23, 24};
-        Integer j4[] = {1, 3, 7, 8, 10, 13, 14, 17, 18, 19, 20, 21, 23, 24, 25};
+        Integer j4[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+        System.out.println("Digite os 15 números do bilhete surpresinha");
+        Scanner b = new Scanner(System.in);
+
+        for (int i = 0; i < 15; i++){
+            j4[i] = b.nextInt();
+        }
         //Colocar aqui abaixo os numeros sorteados, devem ser 15 para nao gerar erro.
         Integer sorteio[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
@@ -17,6 +23,11 @@ public class Main {
         List<Integer> l3 = Arrays.asList(j3);
         List<Integer> l4 = Arrays.asList(j4);
 
+        System.out.println("BILHETES");
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
         int p1 = 0, p2 = 0, p3 = 0, p4 = 0;
 
         System.out.println("Digite os números sorteados");
